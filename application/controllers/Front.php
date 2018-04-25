8967853c8a83f733fb5ae3eee3c4bfa7d895efe9
Tupay <lutfi@mhs.ubharajaya.ac.id>
2018-04-25 19:26:19 +0700

    update front

--- application/controllers/Front.php
+++ application/controllers/Front.php
@@ -6,6 +6,10 @@ class Front extends CI_Controller {
 	public function index()
 	{
 		$data = array();
+		echo 'Contoh Doc : ' . site_url('coba');
+		echo '<p>';
+		echo 'Contoh PDF : ' . site_url('peraturan_mentri');
+		die();
 		$this->load->view('template/header', $data, FALSE);
 		
 	}
