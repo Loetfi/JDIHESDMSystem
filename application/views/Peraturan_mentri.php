54d0451cd55dad7b9d279d943bf6d718e49fbce6
Anggoro <ankghoro@gmail.com>
2018-04-27 07:35:36 +0700

    Update Template

--- application/views/Peraturan_mentri.php
+++ application/views/Peraturan_mentri.php
@@ -5,32 +5,20 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Company-HTML Bootstrap theme</title>
-
-		<!-- Bootstrap -->
-		<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
-		<link rel="stylesheet" href="<?php echo base_url('assets/css/font-awesome.min.css'); ?>">
-		<!--link rel="stylesheet" href="<?php echo base_url('assets/css/animate.css'); ?>">
-		<link rel="stylesheet" href="<?php echo base_url('assets/css/prettyPhoto.css'); ?>"-->
-		<link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>" />
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
+		<link rel="stylesheet" href="<?php echo base_url('asset/css/style.css'); ?>" />
+		<link rel="stylesheet" href="<?php echo base_url('asset/css/modify.css'); ?>" />
 		<script src="<?php echo base_url('assets/js/jquery-2.1.1.min.js'); ?>"></script>
-		<script src="<?php echo base_url('assets/js/jquery.js'); ?>"></script>	
-		<!-- Include all compiled plugins (below), or include individual files as needed -->
-		<script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
-		<!--script src="<?php echo base_url('assets/js/jquery.prettyPhoto.js'); ?>"></script>
-		<script src="<?php echo base_url('assets/js/jquery.isotope.min.js'); ?>"></script>  
-		<script src="<?php echo base_url('assets/js/wow.min.js'); ?>"></script>
-		<script src="<?php echo base_url('assets/js/functions.js'); ?>"></script-->
 		<script>
+			window.addEventListener('load', () => {
+				const loader = document.getElementById('loader');
+				setTimeout(() => {
+					loader.classList.add('fadeOut');
+				}, 100);
+			});
 			function addMenimbang(ele, count) {
 				var counts = count + 1;
-				var element =	'<div id="text-menimbang'+counts+'">'+
-									'<input type="text" name="menimbang[]" class="form-control float-left width-90p" autocomplete="off" placeholder="Menimbang" />'+
+				var element =	'<div class="form-group" id="text-menimbang'+counts+'">'+
+									'<input type="text" name="menimbang[]" class="form-control float-left width-90p" placeholder="Menimbang" />'+
 									'<a href="#" class="fb tool-tip" style="line-height:32px; margin-left:10px;" onclick="addMenimbang(this, '+counts+')"><i class="fa fa-lg fa-plus"></i></a>'+
 								'</div>';
 				$(ele).children().toggleClass('fa-plus fa-minus');
@@ -40,7 +28,7 @@
 			function addMengingat(ele, count) {
 				var counts = count + 1;
 				var element =	'<div id="text-mengingat'+counts+'">'+
-									'<input type="text" name="mengingat[]" class="form-control float-left width-90p" autocomplete="off" placeholder="Mengingat" />'+
+									'<input type="text" name="mengingat[]" class="form-control float-left width-90p" placeholder="Mengingat" />'+
 									'<a href="#" class="fb tool-tip" style="line-height:32px; margin-left:10px;" onclick="addMengingat(this, '+counts+')"><i class="fa fa-lg fa-plus"></i></a>'+
 								'</div>';
 				$(ele).children().toggleClass('fa-plus fa-minus');
@@ -70,24 +58,37 @@
 			}
 		</style>
 	</head>
-	<body>
-		<div style="width:50%; margin: 0 auto">
-			<form style="width:50%; margin: 20px 30px" method="POST" action="<?php echo site_url('Peraturan_mentri/generatePdf'); ?>" target="_blank">
-				<div style="margin-bottom:5px"><textarea name="super_judul" class="form-control" autocomplete="off" placeholder="Judul Dokumen" rows="4"></textarea></div>
-				<div>
-					<div id="text-menimbang1">
-						<input type="text" name="menimbang[]" class="form-control float-left width-90p" autocomplete="off" placeholder="Menimbang" />
-						<a href="#" class="fb tool-tip" style="line-height:32px; margin-left:10px;" onclick="addMenimbang(this, 1)"><i class="fa fa-lg fa-plus"></i></a>
-					</div>
+	<body class="app">
+		<div id="loader">
+			<div class="spinner"></div>
+		</div>
+		<div class="margin-left-right-auto mT-30 width-50p">
+			<form class="width-100p" method="POST" action="<?php echo site_url('Peraturan_mentri/generatePdf'); ?>" target="_blank">
+				<div class="form-group" style="margin-bottom:5px">
+					<textarea name="super_judul" class="form-control" autocomplete="off" placeholder="Judul Dokumen" rows="4"></textarea>
 				</div>
-				<div>
-					<div id="text-mengingat1">
-						<input type="text" name="mengingat[]" class="form-control float-left width-90p" autocomplete="off" placeholder="Mengingat" />
-						<a href="#" class="fb tool-tip" style="line-height:32px; margin-left:10px;" onclick="addMengingat(this, 1)"><i class="fa fa-lg fa-plus"></i></a>
+				<fieldset>
+					<legend>Menimbang</legend>
+					<div>
+						<div class="form-group" id="text-menimbang1">
+							<input type="text" name="menimbang[]" class="form-control float-left width-90p" placeholder="Menimbang" />
+							<a href="#" class="fb tool-tip" style="line-height:32px; margin-left:10px;" onclick="addMenimbang(this, 1)"><i class="fa fa-lg fa-plus"></i></a>
+						</div>
 					</div>
-				</div>
+				</fieldset>
+				<fieldset>
+					<legend>Mengingat</legend>
+					<div>
+						<div class="form-group" id="text-mengingat1">
+							<input type="text" name="mengingat[]" class="form-control float-left width-90p" placeholder="Mengingat" />
+							<a href="#" class="fb tool-tip" style="line-height:32px; margin-left:10px;" onclick="addMengingat(this, 1)"><i class="fa fa-lg fa-plus"></i></a>
+						</div>
+					</div>
+				</fieldset>
 				<div><button type="submit" class="btn btn-success">Generate</button></div>
 			</form>
 		</div>
+		<script type="text/javascript" src="<?php echo base_url('asset/js/vendor.js'); ?>"></script>
+		<script type="text/javascript" src="<?php echo base_url('asset/js/bundle.js'); ?>"></script>
 	</body>
 </html>
