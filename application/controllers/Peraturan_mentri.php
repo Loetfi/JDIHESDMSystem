f8b8ba26790d15bfc3e6284e87a338d9af8f2be2
Anggoro <ankghoro@gmail.com>
2018-04-28 17:02:32 +0700

    Update Template

--- application/controllers/Peraturan_mentri.php
+++ application/controllers/Peraturan_mentri.php
@@ -17,15 +17,25 @@ class Peraturan_mentri extends CI_Controller {
 		$judulMenimbang = '';
 		$judulMengingat = '';
 		for($var = 0; $var < count($post['menimbang']); $var++) {
-			$judulMenimbang .= '<li style="text-align:left;">'.$post['menimbang'][$var].'</li>';
+			$menimbang = ($post['menimbang'][$var] !== '') ? $post['menimbang'][$var] : '&nbsp;' ;
+			$judulMenimbang .= '<li style="margin-bottom:4px; text-align:justify;">'.$menimbang.'</li>';
 		}
 		for($var = 0; $var < count($post['mengingat']); $var++) {
-			$judulMengingat .= '<li style="text-align:left;">'.$post['mengingat'][$var].'</li>';
+			$mengingat = ($post['mengingat'][$var] !== '') ? $post['mengingat'][$var] : '&nbsp;' ;
+			$judulMengingat .= '<li style="margin-bottom:4px; text-align:justify;">'.$mengingat.'</li>';
 		}
-		$html = '<div style="margin:0 1.5em 0 1.5em">
-				 <div style="margin:8em auto 2em auto; text-align:center; text-transform:uppercase; width:50%;">'.$superjudul.'</div>
-				 <div><div style="position:absolute; left:5px;">Menimbang :</div><ol type="a" style="margin:0 auto; width:65%;">'.$judulMenimbang.'</ol></div>
-				 <div><div style="position:absolute; left:5px;">Mengingat :</div><ol type="a" style="margin:0 auto; width:65%;">'.$judulMengingat.'</ol></div>
+		$html = '<div style="font-family:"Bookman Old Style"; font-size:12px;">
+				 <div style="margin:0 1.5em 0 1.5em">
+				 <div style="line-height:25px; margin:8em auto 2em auto; text-align:center; text-transform:uppercase; width:90%;">'.$superjudul.'</div>
+				 <div>
+					<div style="position:absolute; left:55px;">Menimbang</div><div style="position:absolute; left:24%;">:</div>
+					<ol type="a" style="margin:0 auto 0 20%; width:70%;">'.$judulMenimbang.'</ol>
+				 </div>
+				 <div>
+					<div style="position:absolute; left:55px; margin-top:10px;">Mengingat</div><div style="position:absolute; left:24%;">:</div>
+					<ol type="1" style="margin:0 auto 0 20%; width:70%;">'.$judulMengingat.'</ol>
+				 </div>
+				 </div>
 				 </div>';
 		//echo $html; exit;
 		$this->libdompdf->generate($html);
--- application/views/Peraturan_mentri.php
+++ application/views/Peraturan_mentri.php
@@ -1,76 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Company-HTML Bootstrap theme</title>
-		<link rel="stylesheet" href="<?php echo base_url('asset/css/style.css'); ?>" />
-		<link rel="stylesheet" href="<?php echo base_url('asset/css/modify.css'); ?>" />
-		<script src="<?php echo base_url('assets/js/jquery-2.1.1.min.js'); ?>"></script>
-		<script type="text/javascript" src="<?php echo base_url('asset/js/vendor.js'); ?>"></script>
-		<script type="text/javascript" src="<?php echo base_url('asset/js/bundle.js'); ?>"></script>
-		<script>
-			window.addEventListener('load', () => {
-				const loader = document.getElementById('loader');
-				setTimeout(() => {
-					loader.classList.add('fadeOut');
-				}, 100);
-			});
-			function addMenimbang(ele, count) {
-				var counts = count + 1;
-				var element =	'<div class="form-group" id="text-menimbang'+counts+'">'+
-									'<input type="text" name="menimbang[]" class="form-control float-left width-90p" placeholder="Menimbang" />'+
-									'<a href="#" class="fb tool-tip" style="line-height:32px; margin-left:10px;" onclick="addMenimbang(this, '+counts+')"><i class="fa fa-lg fa-plus"></i></a>'+
-								'</div>';
-				$(ele).children().toggleClass('fa-plus fa-minus');
-				$(ele).children().attr("onclick","remove(this)");
-				$(element).last().insertAfter($("#text-menimbang"+count));
-			}
-			function addMengingat(ele, count) {
-				var counts = count + 1;
-				var element =	'<div id="text-mengingat'+counts+'">'+
-									'<input type="text" name="mengingat[]" class="form-control float-left width-90p" placeholder="Mengingat" />'+
-									'<a href="#" class="fb tool-tip" style="line-height:32px; margin-left:10px;" onclick="addMengingat(this, '+counts+')"><i class="fa fa-lg fa-plus"></i></a>'+
-								'</div>';
-				$(ele).children().toggleClass('fa-plus fa-minus');
-				$(ele).children().attr("onclick","remove(this)");
-				$(element).last().insertAfter($("#text-mengingat"+count));
-			}
-			function remove(ele) {
-				$(ele).parent().parent().remove();
-			}
-		</script>
-	</head>
-	<body class="app">
-		<div id="loader">
-			<div class="spinner"></div>
+<script>
+	function addMenimbang(ele, count) {
+		var counts = count + 1;
+		var element =	'<div class="form-group" id="text-menimbang'+counts+'">'+
+							'<input type="text" name="menimbang[]" class="form-control float-left width-90p" placeholder="Menimbang" />'+
+							'<a href="#" class="fb tool-tip" style="line-height:32px; margin-left:10px;" onclick="addMenimbang(this, '+counts+')"><i class="fa fa-lg fa-plus"></i></a>'+
+						'</div>';
+		$(ele).children().toggleClass('fa-plus fa-minus');
+		$(ele).children().attr("onclick","remove(this)");
+		$(element).last().insertAfter($("#text-menimbang"+count));
+	}
+	function addMengingat(ele, count) {
+		var counts = count + 1;
+		var element =	'<div class="form-group" id="text-mengingat'+counts+'">'+
+							'<input type="text" name="mengingat[]" class="form-control float-left width-90p" placeholder="Mengingat" />'+
+							'<a href="#" class="fb tool-tip" style="line-height:32px; margin-left:10px;" onclick="addMengingat(this, '+counts+')"><i class="fa fa-lg fa-plus"></i></a>'+
+						'</div>';
+		$(ele).children().toggleClass('fa-plus fa-minus');
+		$(ele).children().attr("onclick","remove(this)");
+		$(element).last().insertAfter($("#text-mengingat"+count));
+	}
+	function remove(ele) {
+		$(ele).parent().parent().remove();
+	}
+</script>
+<form class="width-100p" method="POST" action="<?php echo site_url('Peraturan_mentri/generatePdf'); ?>" target="_blank">
+	<div class="form-group" style="margin-bottom:5px">
+		<textarea name="super_judul" class="form-control" autocomplete="off" placeholder="Judul Dokumen" rows="4"></textarea>
+	</div>
+	<fieldset>
+		<legend>Menimbang</legend>
+		<div>
+			<div class="form-group" id="text-menimbang1">
+				<input type="text" name="menimbang[]" class="form-control float-left width-90p" placeholder="Menimbang" />
+				<a href="#" class="fb tool-tip" style="line-height:32px; margin-left:10px;" onclick="addMenimbang(this, 1)"><i class="fa fa-lg fa-plus"></i></a>
+			</div>
 		</div>
-		<div class="margin-left-right-auto mT-30 width-50p">
-			<form class="width-100p" method="POST" action="<?php echo site_url('Peraturan_mentri/generatePdf'); ?>" target="_blank">
-				<div class="form-group" style="margin-bottom:5px">
-					<textarea name="super_judul" class="form-control" autocomplete="off" placeholder="Judul Dokumen" rows="4"></textarea>
-				</div>
-				<fieldset>
-					<legend>Menimbang</legend>
-					<div>
-						<div class="form-group" id="text-menimbang1">
-							<input type="text" name="menimbang[]" class="form-control float-left width-90p" placeholder="Menimbang" />
-							<a href="#" class="fb tool-tip" style="line-height:32px; margin-left:10px;" onclick="addMenimbang(this, 1)"><i class="fa fa-lg fa-plus"></i></a>
-						</div>
-					</div>
-				</fieldset>
-				<fieldset>
-					<legend>Mengingat</legend>
-					<div>
-						<div class="form-group" id="text-mengingat1">
-							<input type="text" name="mengingat[]" class="form-control float-left width-90p" placeholder="Mengingat" />
-							<a href="#" class="fb tool-tip" style="line-height:32px; margin-left:10px;" onclick="addMengingat(this, 1)"><i class="fa fa-lg fa-plus"></i></a>
-						</div>
-					</div>
-				</fieldset>
-				<div><button type="submit" class="btn btn-success">Generate</button></div>
-			</form>
+	</fieldset>
+	<fieldset>
+		<legend>Mengingat</legend>
+		<div>
+			<div class="form-group" id="text-mengingat1">
+				<input type="text" name="mengingat[]" class="form-control float-left width-90p" placeholder="Mengingat" />
+				<a href="#" class="fb tool-tip" style="line-height:32px; margin-left:10px;" onclick="addMengingat(this, 1)"><i class="fa fa-lg fa-plus"></i></a>
+			</div>
 		</div>
-	</body>
-</html>
+	</fieldset>
+	<div><button type="submit" class="btn btn-success">Generate</button></div>
+</form>
