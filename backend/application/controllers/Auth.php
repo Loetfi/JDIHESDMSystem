e48d7dd106a9f2562ae34e6472568979e34e7835
Tupay <lutfi@mhs.ubharajaya.ac.id>
2018-04-30 22:11:42 +0800

    update login

---
+++ backend/application/controllers/Auth.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url');
+	}
+
+	public function index()
+	{
+		$this->login();
+	}
+
+	public function login()
+	{
+		$data = array(
+			'contents'	=> 'auth/login',
+			'title'		=> 'Login Sistem'
+		);
+		$this->load->view('template/head', $data, FALSE);
+	}
+
+}
+
+/* End of file Auth.php */
+/* Location: ./application/controllers/Auth.php */
