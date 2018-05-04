d6664128f34eff88f9b0d9535d5875d6c04cadbb
Tupay <lutfi@mhs.ubharajaya.ac.id>
2018-05-04 13:50:36 +0700

    update

--- application/modules/backend/views/auth/login.php
+++ application/modules/backend/views/auth/login.php
@@ -8,7 +8,7 @@
         </div>
         <div class="col-12 col-md-4 peer pX-40 pY-80 h-100 bgc-white scrollable pos-r" style="min-width:320px">
             <h4 class="fw-300 c-grey-900 mB-40">Login</h4>
-            <form action="<?php echo site_url('backend/dashboard/') ?>" method="POST">
+            <form action="<?php echo site_url('apps/') ?>" method="POST">
                 <div class="form-group">
                     <label class="text-normal text-dark">Username / Email</label>
                     <input type="email" class="form-control" placeholder="">
