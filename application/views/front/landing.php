7a2cab32a155935676e9750247c20834f73cdb78
Tupay <lutfi@mhs.ubharajaya.ac.id>
2018-06-12 14:40:33 +0700

    update

---
+++ application/views/front/landing.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <!-- Meta, title, CSS, favicons, etc. -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title> Aplikasi JDIH </title>
+  <link rel="icon" type="image/png" href="images/logopeka.png" />
+
+
+  <link href="http://sipeka.esdm.go.id/themes/esdm/vendors/login/style.css" rel="stylesheet">
+  <link href="http://sipeka.esdm.go.id/themes/esdm/vendors/bootstrap/dist/css/style_sipeka.css" rel="stylesheet"> 
+  <style>
+  @font-face{
+    font-family:"Haettenschweiler";
+    src:url("http://sipeka.esdm.go.id/themes/esdm/fonts/Haettenschweiler.ttf");
+  }
+  /* For width smaller than 400px: */
+  body {
+    background: url(http://sipeka.esdm.go.id/themes/esdm/images/backgroundsipeka22222.png) no-repeat center center fixed; 
+    font-family: Haettenschweiler;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
+
+
+  /* For width 400px and larger: */
+  @media only screen and (min-width: 700px) {
+    body {  
+      background: url(http://sipeka.esdm.go.id/themes/esdm/images/backgroundsipeka22.png) no-repeat center center fixed; 
+      font-family: Haettenschweiler;
+      font-size: 100px;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
+    }  
+  }
+}
+</style>
+</head>
+
+<body id="bodi"> 
+  <div class="container"> 
+    <a href="http://sipeka.esdm.go.id/index.php?go=login">
+      <div class="button">
+        <div class="compass"></div>
+        <div class="msg"><font face="Haettenschweiler" size="20">JDIH</div>
+        </div></a>
+      </div> 
+    </body>
+    
+  
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script> 
+    <script src="http://sipeka.esdm.go.id/themes/esdm/vendors/login/js/index.js"></script>
+    <script src="http://sipeka.esdm.go.id/themes/esdm/vendors/jquery/dist/index.js"></script> 
+    </html>
+
+
+  <script>
+      $('#bodi').on('click',function(){
+        location.href='http://sipeka.esdm.go.id/index.php?go=login';
+      });
+    </script>
+
+
+    <!-- jQuery --> 
