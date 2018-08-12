1ed812f4d278e3de0fd1a40d5811cf70cc89fa95
Unknown <ankghoro@gmail.com>
2018-08-12 21:33:57 +0700

    Update New UU

--- application/views/NewUndang2.php
+++ application/views/NewUndang2.php
@@ -17,12 +17,145 @@
 	background: blue;
 	color: #fff;
 }
+.arrow-right {
+	float: right;
+	line-height: 20px;
+	position: absolute;
+    right: 25px;
+}
 </style>
+<div class="padding">
+	<div class="row">
+		<div class="col-md-3">
+			<div class="box">
+				<div id="left-menus" class="box-header">
+					<span>Aksi</span>
+					<span>
+						<select id="opt-child">
+							<option value="none">None</option>
+							<option value="child">Child</option>
+						</select>
+					</span>
+					<span>
+						<i class="fa fa-save" title="Simpan Format" onclick="saveFormat()"></i>
+						<i class="fa fa-pencil" title="Ubah" onclick="editAction()"></i>
+						<i class="fa fa-plus-circle" title="Tambah" onclick="addAction()"></i>
+						<i class="fa fa-minus-circle" title="Hapus" onclick="minAction(this)"></i>
+					</span>
+				</div>
+				<div class="box-divider m-a-0"></div>
+				<div class="box-body">
+					<div id="rootTable"></div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-9" id="rightPage" style="display:none">
+			<div class="box">
+				<div id="right-menus" class="box-header">
+					<select id="opt-mform">
+						<option value="">Pilih Model Form</option>
+						<option value="mform1">Form Model 1</option>
+						<option value="mform2">Form Model 2</option>
+					</select>
+				</div>
+				<div class="box-divider m-a-0"></div>
+				<div class="box-body">
+					<div id="rootForm">Silahkan Pilih Model Form</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- button modal -->
+<button id="modal-popup" 
+		class="btn white" 
+		data-toggle="modal" 
+		data-target="#m-a-a" 
+		data-ui-toggle-class="flip-y" 
+		data-ui-target="#animate" style="display:none">Open</button>
+
+<div id="m-a-a" class="modal fade animate" data-backdrop="true" aria-hidden="true" style="display:none">
+  <div class="modal-dialog" id="animate" data-ui-class="flip-y" style="width:15%">
+    <div class="modal-content" style="width:100%">
+      <div class="modal-header">
+      	<h5 class="modal-title">Nama Isi</h5>
+      </div>
+      <div class="modal-body text-center" style="padding:10px">
+        <input type="text" name="isi_text" style="width:100%" autofocus />
+        <input type="hidden" name="editflag" value="false" />
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn dark-white p-x-md" onclick="modalBtn(this, 'no')">No</button>
+        <button type="button" class="btn danger p-x-md" onclick="modalBtn(this, 'yes')" id="btnYes">Yes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <script type="text/javascript">
 var filename = 'sideformat_undang2.json';
+
+loadSideFormat();
+function loadSideFormat() {
+	$.ajax({
+		url: '<?php base_url(); ?>files/loadformat',
+		type: 'POST',
+		data: 'filename='+filename,
+		beforeSend: function() {
+			$('#rootTable').html('Load format...');
+		},
+		success: function(data) {
+			var datax = JSON.parse(data), content = '';
+			if(datax.length === 0) {
+				content += '<div class="element" onclick="onFocused(this)">'+
+							'<input type="hidden" class="pointer" value="root" />Judul'+
+							'<input type="hidden" class="namePointer" value="Judul" />'+
+							'<i class="fa fa-arrow-circle-right arrow-right" onclick="loadRightPage()"></i>'+
+							'</div>';
+			}
+			else {
+				for(var i = 0; i < datax.length; i++) {
+					if(datax[i].pointer === 'root') {
+						content += '<div class="element" onclick="onFocused(this)">'+
+									'<input type="hidden" class="pointer" value="root" />'+datax[i].val+
+									'<input type="hidden" class="namePointer" value="Judul" />'+
+									'<i class="fa fa-arrow-circle-right arrow-right" onclick="loadRightPage()"></i>'+
+									'</div>';
+					}
+					else {
+						var varcount = '', csscount = 0;
+						var sub = datax[i].pointer.match(/\d/g);
+						if(sub === null) {
+							csscount = 1 * 10;
+							varcount = '<input type="hidden" class="pointer" value="subroot" />'+datax[i].val;
+						}
+						else {
+							csscount = (parseInt(sub) + 1) * 10;
+							varcount = '<input type="hidden" class="pointer" value="subroot'+(parseInt(sub))+'" />'+datax[i].val;
+						}
+						content += '<div>'+
+									'<div onclick="onFocused(this)" style="margin-left:'+csscount+'px">'+
+									varcount+
+									'<input type="hidden" class="namePointer" value="'+datax[i].val+'" />'+
+									'<i class="fa fa-arrow-circle-right arrow-right" onclick="loadRightPage()"></i>'+
+									'</div>'+
+									'</div>';
+					}
+				}
+			}
+			$('#rootTable').html(content);
+		}
+	});
+}
+
 function onFocused(ele) {
 	$('#rootTable div').removeClass('focused');
 	$(ele).addClass('focused');
+}
+function loadRightPage(setPage='') {
+	$('#rightPage').css('display', 'block');
+	$('#opt-mform').val('').trigger('change');
 }
 function addAction() {
 	var element_value = $('#rootTable').find('.focused').children('input.pointer').val();
@@ -114,6 +247,7 @@ function modalBtn(ele, val) {
 						 '<div onclick="onFocused(this)" style="margin-left:10px">'+
 						 '<input type="hidden" class="pointer" value="subroot" />'+isi+
 						 '<input type="hidden" class="namePointer" value="'+isi+'" />'+
+						 '<i class="fa fa-arrow-circle-right arrow-right" onclick="loadRightPage()"></i>'+
 						 '</div>'+
 						 '</div>';
 				$(newEle).last().insertAfter('.focused');
@@ -127,6 +261,7 @@ function modalBtn(ele, val) {
 							 '<div onclick="onFocused(this)" style="'+getStyle+'">'+
 							 '<input type="hidden" class="pointer" value="'+getFocus+'" />'+isi+
 							 '<input type="hidden" class="namePointer" value="'+isi+'" />'+
+							 '<i class="fa fa-arrow-circle-right arrow-right" onclick="loadRightPage()"></i>'+
 							 '</div>'+
 							 '</div>';
 					var focusParent = $('.focused').parent();
@@ -137,6 +272,7 @@ function modalBtn(ele, val) {
 							 '<div onclick="onFocused(this)" style="margin-left:'+(parseInt(getNum)+10)+'px">'+
 							 '<input type="hidden" class="pointer" value="subroot'+subNum+'" />'+isi+
 							 '<input type="hidden" class="namePointer" value="'+isi+'" />'+
+							 '<i class="fa fa-arrow-circle-right arrow-right" onclick="loadRightPage()"></i>'+
 							 '</div>'+
 							 '</div>';
 					var focusParent = $('.focused').parent();
@@ -149,118 +285,48 @@ function modalBtn(ele, val) {
 	$('input[name="editflag"]').val('false');
 	$(ele).attr("data-dismiss", "modal");
 }
-</script>
-<div class="padding">
-	<div class="row">
-		<div class="col-md-3">
-			<div class="box">
-				<div id="left-menus" class="box-header">
-					<span>Aksi</span>
-					<span>
-						<select id="opt-child">
-							<option value="none">None</option>
-							<option value="child">Child</option>
-						</select>
-					</span>
-					<span>
-						<i class="fa fa-save" title="Simpan Format" onclick="saveFormat()"></i>
-						<i class="fa fa-pencil" title="Ubah" onclick="editAction()"></i>
-						<i class="fa fa-plus-circle" title="Tambah" onclick="addAction()"></i>
-						<i class="fa fa-minus-circle" title="Hapus" onclick="minAction(this)"></i>
-					</span>
-				</div>
-				<div class="box-divider m-a-0"></div>
-				<div class="box-body">
-					<div id="rootTable">
-						<!--div class="element" onclick="onFocused(this)">
-							// disini nanti ada 2 hidden (1 = posisi, 2 = isi)
-							<input type="hidden" class="pointer" value="root" />Judul
-						</div-->
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-9" style="display:none">
-			<div class="box">TES</div>
-		</div>
-	</div>
-</div>
-
-<!-- button modal -->
-<button id="modal-popup" 
-		class="btn white" 
-		data-toggle="modal" 
-		data-target="#m-a-a" 
-		data-ui-toggle-class="flip-y" 
-		data-ui-target="#animate" style="display:none">Open</button>
-
-<div id="m-a-a" class="modal fade animate" data-backdrop="true" aria-hidden="true" style="display:none">
-  <div class="modal-dialog" id="animate" data-ui-class="flip-y" style="width:15%">
-    <div class="modal-content" style="width:100%">
-      <div class="modal-header">
-      	<h5 class="modal-title">Nama Isi</h5>
-      </div>
-      <div class="modal-body text-center" style="padding:10px">
-        <input type="text" name="isi_text" style="width:100%" autofocus />
-        <input type="hidden" name="editflag" value="false" />
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn dark-white p-x-md" onclick="modalBtn(this, 'no')">No</button>
-        <button type="button" class="btn danger p-x-md" onclick="modalBtn(this, 'yes')">Yes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-<script>
-loadSideFormat();
-function loadSideFormat() {
-	$.ajax({
-		url: '<?php base_url(); ?>files/loadformat',
-		type: 'POST',
-		data: 'filename='+filename,
-		beforeSend: function() {
-			$('#rootTable').html('Load format...');
-		},
-		success: function(data) {
-			var datax = JSON.parse(data), content = '';
-			if(datax.length === 0) {
-				content += '<div class="element" onclick="onFocused(this)">'+
-							'<input type="hidden" class="pointer" value="root" />Judul'+
-							'<input type="hidden" class="namePointer" value="Judul" />'+
-							'</div>';
-			}
-			else {
-				for(var i = 0; i < datax.length; i++) {
-					console.log(datax[i]);
-					if(datax[i].pointer === 'root') {
-						content += '<div class="element" onclick="onFocused(this)">'+
-									'<input type="hidden" class="pointer" value="root" />'+datax[i].val+
-									'<input type="hidden" class="namePointer" value="Judul" />'+
-									'</div>';
-					}
-					else {
-						var varcount = '', csscount = 0;
-						var sub = datax[i].pointer.match(/\d/g);
-						if(sub === null) {
-							csscount = 1 * 10;
-							varcount = '<input type="hidden" class="pointer" value="subroot" />'+datax[i].val;
-						}
-						else {
-							csscount = (parseInt(sub) + 1) * 10;
-							varcount = '<input type="hidden" class="pointer" value="subroot'+(parseInt(sub))+'" />'+datax[i].val;
-						}
-						content += '<div>'+
-									'<div onclick="onFocused(this)" style="margin-left:'+csscount+'px">'+
-									varcount+
-									'<input type="hidden" class="namePointer" value="'+datax[i].val+'" />'+
-									'</div>'+
-									'</div>';
-					}
-				}
-			}
-			$('#rootTable').html(content);
-		}
-	});
+function mform1() {
+	return '<textarea data-toggle="tooltip"'+
+			'title="title" type="text" name=""'+
+			'class="form-control" placeholder="Menimbang" rows="6" required></textarea>';
 }
+function mform2() {
+	return '<div style="display:inline-block; width:100%;">'+
+			'<div style="float:left; width:20%">'+
+			'<input type="text" name="" class="form-control" placeholder="Pointer" data-toggle="tooltip" title="" />'+
+			'<select name="" class="form-control" style="cursor:pointer; margin-top:10px;" data-toggle="tooltip" title="title">'+
+			'<option value="continue">Continues Page</option>'+
+			'<option value="newP">Next Page Portrait</option><option value="newL">Next Page Landscape</option>'+
+			'</select>'+
+			'<select name="" class="form-control" style="cursor:pointer; margin-top:10px;">'+
+			'<option value="0">SubLevel 0</option>'+
+			'<option value="1">SubLevel 1</option>'+
+			'<option value="2">SubLevel 2</option>'+
+			'<option value="3">SubLevel 3</option>'+
+			'<option value="4">SubLevel 4</option>'+
+			'</select>'+
+			'</div>'+
+			'<div style="width:100%">'+
+			'<textarea data-toggle="tooltip" title="title" type="text" name="" class="form-control" placeholder="place" rows="6" style="float:left; margin-left:5px; width:75%;" required></textarea>'+
+			'</div>'+
+			'</div>';
+}
+
+$('#opt-mform').change(function() {
+	if($(this).val() === '') {
+		$('#rootForm').html('Silahkan Pilih Model Form');
+	}
+	if($(this).val() === 'mform1') {
+		$('#rootForm').html(mform1());
+	}
+	else if($(this).val() === 'mform2') {
+		$('#rootForm').html(mform2());
+	}
+});
+
+$('input[name="isi_text"]').keyup(function(e) {
+    if(e.keyCode === 13) {
+        $('#btnYes').click();
+    }
+});
 </script>
